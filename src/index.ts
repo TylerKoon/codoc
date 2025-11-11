7e321c72bd3022eef6a01caede1cb4d3e555ca0e
Tyler Koon <tylerkoon@github.com>
2025-11-10 21:19:45 -0800

    Exploring OpenAPI library

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import OpenAI from "openai";
+
 
 async function run(): Promise<void> {
   try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput("who-to-greet");
-    core.info(`Hello ${nameToGreet}!`);
+    // Check if the PR is in draft (if it is, exit early)
+    const pr = github.context.payload.pull_request;
+    if (pr && pr.draft) {
+      core.info("Pull request is not ready for review. Exiting...");
+      return;
+    }
 
-    // Get the current time and set it as an output variable
-    const time = new Date().toTimeString();
-    core.setOutput("time", time);
+    // Get GH Token
+    const token = process.env['GITHUB_TOKEN']
+    if (token === undefined) {
+      throw new Error('GITHUB_TOKEN is not set')
+    }
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    core.info(`The event payload: ${payload}`);
+    // Read inputs
+    const endpoint = core.getInput('endpoint')
+    const model = core.getInput('model') || 'gpt-4o'
+    const maxTokens = parseInt(core.getInput('max_tokens')) || 1024
+
+    // Create client
+    const client = new OpenAI({
+      apiKey: token,
+      baseURL: endpoint,
+    });
+
+    // Run inference
+    const response = await client.chat.completions.create({
+      model: model,
+      messages: [
+        { role: "system", content: "You are a bot that runs in a GitHub Action. Your purpose is to look at the diff for the PR and determine if there have been significant changes to the repository that warrant a change to the current version of documentation." },
+        {role: "user", content: "Here is the diff for the current PR:\n\n" + pr?.body + "\n\nBased on this diff, please respond with a concise summary of the changes and whether they require updates to the documentation. Do not provide recommended documentation changes, just say if documentation changes are needed."}
+      ],
+      max_tokens: maxTokens,
+    });
+
+    core.info(`Response: ${response.choices[0].message.content}`);
+      
+    
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
